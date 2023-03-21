74adc15ab793c89195f29c3f303187a5fb5da712
Linas Norkus <linasnorkus92@gmail.com>
2023-03-21 21:28:02 +0200

    fully working "loot" inventory manager

--- Inventory Management App/Form1.Designer.cs
+++ Inventory Management App/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Inventory_Management_App
 {
-    partial class Form1
+    partial class InvintoryManagementStudio
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,244 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.priceTextBox = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.newButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.descriptionTextBox = new System.Windows.Forms.TextBox();
+            this.quantityTextBox = new System.Windows.Forms.TextBox();
+            this.skuTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.categoryTextBox = new System.Windows.Forms.ComboBox();
+            this.skuLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.descriptionLabel = new System.Windows.Forms.Label();
+            this.quantityLabel = new System.Windows.Forms.Label();
+            this.priceLabel = new System.Windows.Forms.Label();
+            this.categoryLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1039, 51);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Awesome Inventory Management App For Legendary Loot";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // priceTextBox
+            // 
+            this.priceTextBox.Location = new System.Drawing.Point(19, 137);
+            this.priceTextBox.Name = "priceTextBox";
+            this.priceTextBox.Size = new System.Drawing.Size(333, 20);
+            this.priceTextBox.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(19, 210);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1033, 334);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // newButton
+            // 
+            this.newButton.BackColor = System.Drawing.Color.Ivory;
+            this.newButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newButton.Location = new System.Drawing.Point(19, 163);
+            this.newButton.Name = "newButton";
+            this.newButton.Size = new System.Drawing.Size(333, 41);
+            this.newButton.TabIndex = 3;
+            this.newButton.Text = "New";
+            this.newButton.UseVisualStyleBackColor = false;
+            this.newButton.Click += new System.EventHandler(this.newButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.BackColor = System.Drawing.Color.Ivory;
+            this.deleteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteButton.Location = new System.Drawing.Point(719, 163);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(333, 41);
+            this.deleteButton.TabIndex = 4;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = false;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.BackColor = System.Drawing.Color.Ivory;
+            this.saveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveButton.Location = new System.Drawing.Point(371, 163);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(333, 41);
+            this.saveButton.TabIndex = 5;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = false;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // descriptionTextBox
+            // 
+            this.descriptionTextBox.Location = new System.Drawing.Point(371, 137);
+            this.descriptionTextBox.Name = "descriptionTextBox";
+            this.descriptionTextBox.Size = new System.Drawing.Size(333, 20);
+            this.descriptionTextBox.TabIndex = 6;
+            // 
+            // quantityTextBox
+            // 
+            this.quantityTextBox.Location = new System.Drawing.Point(719, 137);
+            this.quantityTextBox.Name = "quantityTextBox";
+            this.quantityTextBox.Size = new System.Drawing.Size(333, 20);
+            this.quantityTextBox.TabIndex = 7;
+            // 
+            // skuTextBox
+            // 
+            this.skuTextBox.Location = new System.Drawing.Point(19, 95);
+            this.skuTextBox.Name = "skuTextBox";
+            this.skuTextBox.Size = new System.Drawing.Size(333, 20);
+            this.skuTextBox.TabIndex = 8;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(371, 95);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(333, 20);
+            this.nameTextBox.TabIndex = 9;
+            // 
+            // categoryTextBox
+            // 
+            this.categoryTextBox.FormattingEnabled = true;
+            this.categoryTextBox.Items.AddRange(new object[] {
+            "Sword",
+            "Mace",
+            "Knife",
+            "Bow",
+            "Spear",
+            "Crossbow",
+            "Saber",
+            "Poleaxe",
+            "Axe",
+            "Hammer",
+            "Stave",
+            "Glavius"});
+            this.categoryTextBox.Location = new System.Drawing.Point(719, 95);
+            this.categoryTextBox.Name = "categoryTextBox";
+            this.categoryTextBox.Size = new System.Drawing.Size(333, 21);
+            this.categoryTextBox.TabIndex = 10;
+            // 
+            // skuLabel
+            // 
+            this.skuLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.skuLabel.Location = new System.Drawing.Point(19, 73);
+            this.skuLabel.Name = "skuLabel";
+            this.skuLabel.Size = new System.Drawing.Size(333, 19);
+            this.skuLabel.TabIndex = 12;
+            this.skuLabel.Text = "SKU:";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.Location = new System.Drawing.Point(368, 76);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(333, 16);
+            this.nameLabel.TabIndex = 13;
+            this.nameLabel.Text = "NAME:";
+            // 
+            // descriptionLabel
+            // 
+            this.descriptionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.descriptionLabel.Location = new System.Drawing.Point(368, 118);
+            this.descriptionLabel.Name = "descriptionLabel";
+            this.descriptionLabel.Size = new System.Drawing.Size(333, 16);
+            this.descriptionLabel.TabIndex = 14;
+            this.descriptionLabel.Text = "DESCRIPTION:";
+            // 
+            // quantityLabel
+            // 
+            this.quantityLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quantityLabel.Location = new System.Drawing.Point(719, 119);
+            this.quantityLabel.Name = "quantityLabel";
+            this.quantityLabel.Size = new System.Drawing.Size(333, 16);
+            this.quantityLabel.TabIndex = 15;
+            this.quantityLabel.Text = "QUANTITY:";
+            // 
+            // priceLabel
+            // 
+            this.priceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.priceLabel.Location = new System.Drawing.Point(17, 118);
+            this.priceLabel.Name = "priceLabel";
+            this.priceLabel.Size = new System.Drawing.Size(333, 16);
+            this.priceLabel.TabIndex = 16;
+            this.priceLabel.Text = "PRICE:";
+            // 
+            // categoryLabel
+            // 
+            this.categoryLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.categoryLabel.Location = new System.Drawing.Point(719, 76);
+            this.categoryLabel.Name = "categoryLabel";
+            this.categoryLabel.Size = new System.Drawing.Size(333, 16);
+            this.categoryLabel.TabIndex = 17;
+            this.categoryLabel.Text = "CATEGORY:";
+            // 
+            // InvintoryManagementStudio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
             this.ClientSize = new System.Drawing.Size(1064, 556);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.categoryLabel);
+            this.Controls.Add(this.priceLabel);
+            this.Controls.Add(this.quantityLabel);
+            this.Controls.Add(this.descriptionLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.skuLabel);
+            this.Controls.Add(this.categoryTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.skuTextBox);
+            this.Controls.Add(this.quantityTextBox);
+            this.Controls.Add(this.descriptionTextBox);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.newButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.priceTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "InvintoryManagementStudio";
+            this.Text = "InvintoryManagementStudio";
+            this.Load += new System.EventHandler(this.InvintoryManagementStudio_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox priceTextBox;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button newButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.TextBox descriptionTextBox;
+        private System.Windows.Forms.TextBox quantityTextBox;
+        private System.Windows.Forms.TextBox skuTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.ComboBox categoryTextBox;
+        private System.Windows.Forms.Label skuLabel;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label descriptionLabel;
+        private System.Windows.Forms.Label quantityLabel;
+        private System.Windows.Forms.Label priceLabel;
+        private System.Windows.Forms.Label categoryLabel;
     }
 }
 
--- Inventory Management App/Form1.cs
+++ Inventory Management App/Form1.cs
@@ -10,11 +10,95 @@ using System.Windows.Forms;
 
 namespace Inventory_Management_App
 {
-    public partial class Form1 : Form
+    public partial class InvintoryManagementStudio : Form
     {
-        public Form1()
+        DataTable inventory = new DataTable();
+        public InvintoryManagementStudio()
         {
             InitializeComponent();
+        }
+
+        /*  i fucked up and double clicked :(
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }*/
+
+        private void newButton_Click(object sender, EventArgs e)
+        {
+            skuTextBox.Text = string.Empty;
+            nameTextBox.Text = string.Empty;
+            priceTextBox.Text = string.Empty;
+            quantityTextBox.Text = string.Empty;
+            descriptionTextBox.Text = string.Empty;
+            categoryTextBox.SelectedIndex = -1;
+            
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            // good to use try and catch with "deletes" because of trying to delete empty n stuff
+            try
+            {
+                inventory.Rows[dataGridView1.CurrentCell.RowIndex].Delete();
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine($"Error: {error}");
+                //throw;
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            // save all the values from our fields into variables
+            String sku = skuTextBox.Text;
+            String name = nameTextBox.Text;
+            String price = priceTextBox.Text;
+            String quantity = quantityTextBox.Text;
+            String description = descriptionTextBox.Text;
+            String category = (string)categoryTextBox.SelectedItem;
+
+            // Add these values to the datatable
+            inventory.Rows.Add(sku, name, category, price, description, quantity);
+
+            // Clears the textbox fields after save
+            newButton_Click(sender, e);
+
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                skuTextBox.Text = inventory.Rows[dataGridView1.CurrentCell.RowIndex].ItemArray[0].ToString();
+                nameTextBox.Text = inventory.Rows[dataGridView1.CurrentCell.RowIndex].ItemArray[1].ToString();
+                priceTextBox.Text = inventory.Rows[dataGridView1.CurrentCell.RowIndex].ItemArray[3].ToString();
+                descriptionTextBox.Text = inventory.Rows[dataGridView1.CurrentCell.RowIndex].ItemArray[4].ToString();
+                quantityTextBox.Text = inventory.Rows[dataGridView1.CurrentCell.RowIndex].ItemArray[5].ToString();
+
+                String itemToLookFor = inventory.Rows[dataGridView1.CurrentCell.RowIndex].ItemArray[2].ToString();
+                categoryTextBox.SelectedIndex = categoryTextBox.Items.IndexOf(itemToLookFor);
+
+
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine($"There has been an error: {error}");
+                //throw;
+            }
+        }
+
+        private void InvintoryManagementStudio_Load(object sender, EventArgs e)
+        {
+            inventory.Columns.Add("SKU");
+            inventory.Columns.Add("NAME");
+            inventory.Columns.Add("CATEGORY");
+            inventory.Columns.Add("PRICE");
+            inventory.Columns.Add("DESCRIPTION");
+            inventory.Columns.Add("QUANTITY");
+
+            dataGridView1.DataSource = inventory;
         }
     }
 }
